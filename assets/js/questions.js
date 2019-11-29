5050cb0bdcd338fce984c3e72311dc81c6b92de9
rpharr <rpharr@richmond.edu>
2019-11-29 12:02:20 -0500

    timer loop established

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -7,9 +7,16 @@
          title: "The condition in an if / else statement is enclosed within ____.",
          choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
          answer: "parentheses"
+     },
+     {
+         title: "Question 1",
+         choices: ["sdfsdfsdf", "erwgergwergewg", "fdhdfghdfgh", "dfghdfghs"],
+         answer: "erwgergwergewg"
+     },
+     {
+         title: "Question 2",
+         choices: ["sdfsdfsdf", "erwgergwergewg", "fdhdfghdfgh", "dfghdfghs"],
+         answer: "erwgergwergewg"
      }
- ];
 
- module.exports = {
-     questions
- }
+ ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,67 @@
-const questions = require("./questions.js");
+var startBtn = document.querySelector("#startBtnID");
+var timerSpan = document.querySelector("#timerSpanID");
+var promtSpan = document.querySelector("#promtSpanID");
+var choicesUL = document.querySelector("#choicesULID");
+var resultSpan = document.querySelector("#resultSpanID");
 
-console.log(questions.questions[0].title);
+// console.log(questions[0].title);
+
+var quiz = false;
+var highscores = JSON.parse(localStorage.getItem("highscores"));
+if (highscores === null) {
+    highscores = [];
+}
+
+var totalSeconds = 75;
+var secondsLeft = 0;
+var interval;
+var score;
+var quizArray;
+
+function startTimer() {
+
+    secondsLeft = totalSeconds;
+
+    interval = setInterval(function() {
+        secondsLeft--;
+
+        timerSpan.textContent = secondsLeft;
+
+        if (quiz) {
+            //display question
+            var i = Math.floor(Math.random() * quizArray.length);
+            var current = quizArray[current];
+            console.log(current);
+
+            promtSpan.textContent = current.title;
+            //display mulitple choices
+            current.choices.forEach(element => {
+                //create list element and append to choicesUL
+                var li = document.createElement("li");
+                li.textContent = element;
+                choicesUL.appendChild(li);
+            });
+
+            //calc and display result
+
+            //remove question from quiz array
+        }
+
+        if (secondsLeft === 0) {
+            clearInterval(interval);
+            // quiz over
+            alert("Time's up!");
+            quiz = false;
+        }
+
+    }, 100);
+}
+
+function startQuiz() {
+    quiz = true;
+    quizArray = questions.slice();
+    quizArray.sort(function(a, b) { return 0.5 - Math.random() });
+    startTimer();
+}
+
+startBtn.addEventListener("click", startQuiz);
